f4942a640a2ab983bf779b2460805027649fd052
Franco <francoherrera_8@hotmail.com.ar>
2023-05-16 22:40:47 -0300

    reducer getById

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -6,7 +6,7 @@ const initialState = () => {
     //const initialCart = cartInLocalStorage ? JSON.parse(cartInLocalStorage) : []
     return {
         allProductos: [],
-        detail: [],
+        detail: {},
         
     };
 };
