b57e46abc8bf12bff832e48cfa6992b4629d52bc
Franco <francoherrera_8@hotmail.com.ar>
2023-05-19 21:46:14 -0300

    cart detalles

--- src/componentes/Cart/cart.jsx
+++ src/componentes/Cart/cart.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import image from "../../assets/images/Nike.jpg";
 import "./cart.css"
 import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
@@ -10,10 +9,12 @@ const Cart = () => {
   const dispatch = useDispatch();
   const carro = useSelector((state)=> state.cart);
   //const cart = localStorage.getItem('cart');
-const quantity = useSelector((state)=>state.numberCart)
+ const quantity = useSelector((state)=>state.numberCart)
 
 console.log(carro);
-//let price = carro.cart.map(e=>e.price*e.quantity).reduce((a,current)=>a+current,0)
+console.log("numberCart:",quantity);
+
+//let price = carro.map(e=>e.price*e.cantidad).reduce((a,current)=>a+current,0)
 
   return (
   <div className="carritos show">
@@ -45,51 +46,6 @@ console.log(carro);
               </div>
                 )
               })}
-              {/* <div className="carrito__item">
-                  <img className="imgCart" src={image} alt="" />
-                  <div>
-                     <h3 className="titleProduct">Title Product</h3>
-                     <p className="price">$1000</p>
-                  </div>
-              <div className="quantityContainer">
-                 <box-icon name="up-arrow" type="solid"></box-icon>
-                 <p className="cantidad"> 5</p>
-                 <box-icon name="down-arrow" type="solid"></box-icon>
-              </div>
-              <div className="remove__item">
-                 <box-icon name="trash"></box-icon>
-              </div>
-              </div>
-              <div className="carrito__item">
-                  <img className="imgCart" src={image} alt="" />
-                  <div>
-                     <h3 className="titleProduct">Title Product</h3>
-                     <p className="price">$1000</p>
-                  </div>
-              <div className="quantityContainer">
-                 <box-icon name="up-arrow" type="solid"></box-icon>
-                 <p className="cantidad"> 5</p>
-                 <box-icon name="down-arrow" type="solid"></box-icon>
-              </div>
-              <div className="remove__item">
-                 <box-icon name="trash"></box-icon>
-              </div>
-              </div>
-              <div className="carrito__item">
-                  <img className="imgCart" src={image} alt="" />
-                  <div>
-                     <h3 className="titleProduct">Title Product</h3>
-                     <p className="price">$1000</p>
-                  </div>
-              <div className="quantityContainer">
-                 <box-icon name="up-arrow" type="solid"></box-icon>
-                 <p className="cantidad"> 5</p>
-                 <box-icon name="down-arrow" type="solid"></box-icon>
-              </div>
-              <div className="remove__item">
-                 <box-icon name="trash"></box-icon>
-              </div>
-              </div> */}
          </div>
          <div className="carrito__footer">
              <h3>Total:$5211</h3>
